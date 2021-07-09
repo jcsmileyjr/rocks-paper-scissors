af297ede13cfc5f003294b72e26fa06768fa43c6
Smiley <jcsmileyjr@gmail.com>
2021-07-09 16:17:02 -0500

    GameArea image & label styling

--- src/components/gameArea/GameArea.js
+++ src/components/gameArea/GameArea.js
@@ -10,15 +10,15 @@ const GameArea = () => {
             <div className="gameArea__image-gallery--style">
                 <section>
                     <img className="gameArea__image-style" id="rock" src={Rock} alt="A rock with a human looking face" />
-                    <label for="rock">Rock</label>
+                    <label className="gameArea__label--style" for="rock">Rock</label>
                 </section>
                 <section>
                     <img className="gameArea__image-style" id="paper" src={Paper} alt="A peice of paper" />
-                    <label for="paper">Paper</label>
+                    <label className="gameArea__label--style" for="paper">Paper</label>
                 </section>
                 <section>
                     <img className="gameArea__image-style" id="scissors" src={Scissors} alt="A single scissors with black handels" />
-                    <label for="scissors">Scissors</label>
+                    <label className="gameArea__label--style" for="scissors">Scissors</label>
                 </section>
             </div>
             <div className="gameAreaButton__containter--style">
