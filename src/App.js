41420f816cbe35572d6f69422dd13b371182849a
Smiley <jcsmileyjr@gmail.com>
2021-07-09 14:02:52 -0500

    Feature: Design responsive Title area

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Title from '../src/components/title/Title';
 
 function App() {
   return (
     <div className="App">
-
+      <Title />
+      <main>
+        Hello world
+      </main>
     </div>
   );
 }
---
+++ src/components/title/Title.js
@@ -0,0 +1,11 @@
+import './title.css';
+const Title = () => {
+    return(
+        <section className="title__container--style">
+            <p> Paper, Rock, Scissors Game </p>
+        </section>
+    );
+}
+
+export default Title;
+
