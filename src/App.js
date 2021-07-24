fc881e1e5a951c2b9cb01350810b318a51b260b9
Smiley <jcsmileyjr@gmail.com>
2021-07-24 18:26:27 -0500

    Feature: Show wins

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
   const [score, setScore] = useState(0);
   const [showRules, setShowRules] = useState(false);
   const [playerLives, setPlayerLives] = useState(3);
+  const [playerWins, setPlayerWins] = useState(0);
+
 
   const playGame = () => {
     setShowRules(true);
@@ -46,14 +48,17 @@ function App() {
       results.playerSummary = "Win";
       results.computerSummary = "Lose";
       setScore(score + 10);
+      setPlayerWins(playerWins + 1);
     }else if(playerChoice === "Scissors" && computerChoice === "Paper"){
       results.playerSummary = "Win";
       results.computerSummary = "Lose";
       setScore(score + 10);
+      setPlayerWins(playerWins + 1);
     }else if(playerChoice === "Paper" && computerChoice === "Rock"){
       results.playerSummary = "Win";
       results.computerSummary = "Lose";
       setScore(score + 10);
+      setPlayerWins(playerWins + 1);
     }else{
       results.playerSummary = "Lose";
       results.computerSummary = "Win";
@@ -86,7 +91,7 @@ function App() {
       }
       { showRules &&       
         <main>
-          <Score score={score} lives={playerLives} />
+          <Score score={score} lives={playerLives} wins={playerWins} />
           {!showGameSummery  &&
             <GameArea getPlayerchoice={getPlayerchoice} play={determineWinner} />
           }
--- src/components/score/Score.js
+++ src/components/score/Score.js
@@ -1,11 +1,19 @@
 import './score.css';
 
-const Score = ({score, lives}) => {
+const Score = ({score, lives, wins}) => {
     return(
         <section className="score__container--layout">
             <div className="score__containter--style">
+                <h2>Timer</h2>
+                <p>{score}</p>
+            </div>
+            <div className="score__containter--style">
                 <h2>Score</h2>
                 <p>{score}</p>
+            </div>
+            <div className="score__containter--style">
+                <h2>Wins</h2>
+                <p>{wins}</p>
             </div>
             <div className="score__containter--style">
                 <h2>Lives</h2>
